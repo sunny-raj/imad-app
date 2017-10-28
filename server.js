af883860054962c85f838774afb29a96f1513e6c
sunny-raj <31388930+sunny-raj@users.noreply.github.com>
2017-10-28 23:18:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical one',function (req,res){
+    res.send('artical one is here');
+});
+
+app.get('/artical two',function (req,res){
+    res.send('artical two is here');
+});
+
+app.get('/artical three',function (req,res){
+    res.send('artical three is here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
