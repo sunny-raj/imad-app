cac41b4d676d82c0f2caef30e97c4d3cbdd10c1d
sunny-raj <31388930+sunny-raj@users.noreply.github.com>
2017-10-28 23:21:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical one',function (req,res){
+app.get('/artical-one',function (req,res){
     res.send('artical one is here');
 });
 
-app.get('/artical two',function (req,res){
+app.get('/artical-two',function (req,res){
     res.send('artical two is here');
 });
 
-app.get('/artical three',function (req,res){
+app.get('/artical-three',function (req,res){
     res.send('artical three is here');
 });
 
