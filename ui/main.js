097c7e7d82346c0a550930f890ea863626cc50d7
sunny-raj <31388930+sunny-raj@users.noreply.github.com>
2017-11-17 01:15:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New-Value';
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick= function() {
-    img.style.marginLeft = '100 px';
+    var interval=setInterval(moveRight,100);
     
-
+    
 };
