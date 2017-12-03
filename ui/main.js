ab7556e7e2d498538943788cf268a47914f17048
sunny-raj <31388930+sunny-raj@users.noreply.github.com>
2017-12-03 15:40:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,21 @@ button.onclick = function(){
   request.open('GET','http://http://sunnyrajpal87.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+//submitt name
+var nameInput = document.getElementByid('name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  //make a request to the srver and send name
+  //capture the list of name and ender it as a list
+  
+  var names = ['name1','name2','name3'];
+  var list='';
+  for(var i=0; i<name.lenght; i++){
+      list+='<li>'+ name[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+    };
